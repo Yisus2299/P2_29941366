28db355839a44700d634e92da614562f6404111b
Yisus2299 <103217823+Yisus2299@users.noreply.github.com>
2022-05-28 23:41:07 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const sqlite3=require('sqlite3').verbose();
 const path = require('path');
+const XMLHttpRequest = require('xhr2');
+const SECRET_KEY = '6LdsNSggAAAAAJyUq-YGe0sDqL26lVQwkPiia2q-';
+const fetch = require('node-fetch');
 
 const basededatos=path.join(__dirname,"basededatos","basededatos.db");
 const bd=new sqlite3.Database(basededatos, err =>{ 
@@ -11,8 +14,8 @@ if (err){
 	console.log("db only");
 }
 })
-const port = process.env.PORT || 3000;
-const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT);";
+
+const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT, country VARCHAR(20);";
 
 bd.run(create,err=>{
 	if (err){
@@ -28,28 +31,52 @@ router.get('/contactos',(req,res)=>{
 			if (err){
 				return console.error(err.message);
 			}else{
-			res.render("contactos.ejs",{tarea:rows});
+			res.render("contactos.ejs",{datos:rows});
 			}
 	})
 })
 
-
+//Envio POST del Formulario.
 router.post('/',(req,res)=>{
-  	var hoy = new Date();
-  	var horas = hoy.getHours();
-  	var minutos = hoy.getMinutes();
-  	var segundos = hoy.getSeconds()
-  	var hora = horas + ':' + minutos + ':' + segundos + ' '
+  	const response_key = req.body["g-recaptcha-response"];
+  	const secret_key = process.env.KEY_PRIVATE;
+  	const url = 
+	`https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+  	fetch(url, {
+    	method: "post",
+  	})
+    	.then((response) => response.json())
+    	.then((google_response) => {
+			//Si se verifica el captcha, automaticamente se hace envia los datos a la Base de Datos
+		if (google_response.success == true) {
+	//Obtener la fecha/hora
+	var hoy = new Date();
+	var horas = hoy.getHours();
+	var minutos = hoy.getMinutes();
+	var segundos = hoy.getSeconds();
+  	var hora = horas + ':' + minutos + ':' + segundos + ' ';
   	var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '//' + hora;
-	var ip = req.headers["x-forwarded-for"];
-  	if (ip){
-    var list = ip.split(",");
-    ip= list[list.length-1];
- 	 } else {
-	  ip = req.connection.remoteAddress;
-  	}
-	const sql="INSERT INTO contactos(nombre, email, comentario, fecha ,ip) VALUES (?,?,?,?,?)";
-	const nuevos_mensajes=[req.body.nombre, req.body.email, req.body.comentario,fecha,ip];
+	  //////////////Obtener la IP publica////////////////
+	  var ip = req.headers["x-forwarded-for"];
+	  if (ip){
+		var list = ip.split(",");
+		ip = list[list.length-1];
+	 } else {
+		ip = req.connection.remoteAddress;
+	  }
+	  ////////////Obtener el Pais//////////////
+	  var XMLHttp = new XMLHttpRequest();
+	  XMLHttp.onreadystatechange = function(){
+	  if(this.readyState == 4 && this.status == 200) {
+		  var ipwhois = JSON.parse(this.responseText); 
+		  var country = ipwhois.country 
+		  var countryCode = ipwhois.country_code
+		  var clientCountry = country + '(' + countryCode + ')'
+		}	
+
+		//Ingreso de los registros hacia la Base de Datos
+	const sql="INSERT INTO contactos(nombre, email, comentario, fecha,ip,country) VALUES (?,?,?,?,?,?)";
+	const nuevos_mensajes=[req.body.nombre, req.body.email, req.body.comentario,fecha,ip,clientCountry];
 	bd.run(sql, nuevos_mensajes, err =>{
 	if (err){
 		return console.error(err.message);
@@ -58,21 +85,19 @@ router.post('/',(req,res)=>{
 		res.redirect("/");
 		}
 	})
-});
+} 
+	}
+}); //=> Llave qué cierra el "if" para obtener el pais
+
+//Obtener el Pais desde la API con la IP.
+XMLHttp.open('GET', 'https://ipwho.is/' + ip, true); 
+XMLHttp.send();		
 
 
 router.get('/',(req,res)=>{
-	res.render('index.ejs',{tarea:{}})
+	res.render('index.ejs',{datos:{}})
 });
 
 
 
-module.exports = router;
-
-
-
-
-
-
-
-
+module.exports = router});
